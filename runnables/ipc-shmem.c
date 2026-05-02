5f5875bb86e8319091597a348ef4e6bdf31b86e9
artsky <artsky@node-0.artsky-303845.rise-pg0.utah.cloudlab.us>
2026-05-02 14:42:25 -0600

    modified ipc_shmem file and removed usage for USE_TRANSPOSE and WORKERS. Can still modify matrix size. Removed unused safe_read function and fixed memory freeing issues

--- runnables/ipc-shmem.c
+++ runnables/ipc-shmem.c
@@ -22,46 +22,33 @@ void safe_read(int fd, void *buf, size_t count);
 void arg_check(int argc, char *argv[]);
 
 int main(int argc, char *argv[]) {
-    int MATRIX_SIZE, WORKERS;
-    bool USE_TRANSPOSE;
-
+    int MATRIX_SIZE;
     arg_check(argc, argv);
-
     MATRIX_SIZE = atoi(argv[1]);
-    WORKERS = atoi(argv[2]);
-    USE_TRANSPOSE = (strcmp(argv[3], "true") == 0);
 
     srand(time(NULL));
     struct timespec start, end; 
 
-    long num_workers = WORKERS ? WORKERS : sysconf(_SC_NPROCESSORS_ONLN);
+    long num_workers = sysconf(_SC_NPROCESSORS_ONLN);
     int base_rows = MATRIX_SIZE / num_workers; 
     int remainder = MATRIX_SIZE % num_workers;
     int start_row, end_row; 
     
-    
     // unlike with pipes, the matrices have been allocated and delivered into a shared space.
     int **A = malloc_sq_matrix(MATRIX_SIZE);
     int **B = malloc_sq_matrix(MATRIX_SIZE);
-    int **B_T = NULL; 
+    int **B_T = malloc_sq_matrix(MATRIX_SIZE); 
     int **C = malloc_sq_matrix_shared(MATRIX_SIZE);
 
-    if (A == NULL || B == NULL || C == NULL) {
-        fprintf(stderr, "Allocation for matrices A, B, C failed. Exiting...");
+    if (A == NULL || B == NULL || C == NULL || B_T == NULL) {
+        fprintf(stderr, "Allocation for matrices A, B, B_T, C failed. Exiting...");
         return 1; 
     }
     
     rand_init_sq_matrix(A, MATRIX_SIZE);
     rand_init_sq_matrix(B, MATRIX_SIZE);
     zero_init_sq_matrix(C, MATRIX_SIZE);
-
-    if (USE_TRANSPOSE) {
-        B_T = malloc_sq_matrix(MATRIX_SIZE);
-        if (B_T == NULL) {
-            fprintf(stderr, "Allocation for transposed matrix failed. Exiting...");
-        }
-        transpose_sq_matrix(B, B_T, MATRIX_SIZE);
-    }
+    transpose_sq_matrix(B, B_T, MATRIX_SIZE);
 
     clock_gettime(CLOCK_MONOTONIC, &start);
 
@@ -82,8 +69,7 @@ int main(int argc, char *argv[]) {
             
             // Because each process is working on it's own row of the C matrix, there is no contentions therefore locks are not needed.
             for (int r = start_row; r < end_row; r++) {
-                if (USE_TRANSPOSE) mult_sq_matrices_row_transposed(r, A, B_T, C, MATRIX_SIZE);
-                else mult_sq_matrices_row(r, A, B, C, MATRIX_SIZE);
+                mult_sq_matrices_row_transposed(r, A, B_T, C, MATRIX_SIZE);
             }
             exit(0); 
         }
@@ -111,47 +97,24 @@ int main(int argc, char *argv[]) {
     bool verified = verified_matrix(A, B, C, MATRIX_SIZE);   
     print_stats(MATRIX_SIZE, num_workers, verified, total_time_sec);
 
-    free_sq_matrix_shared(A, MATRIX_SIZE);
-    free_sq_matrix_shared(B, MATRIX_SIZE);
+    free_sq_matrix(A);
+    free_sq_matrix(B);
+    free_sq_matrix(B_T);
     free_sq_matrix_shared(C, MATRIX_SIZE);
-    if (B_T) free_sq_matrix_shared(B_T, MATRIX_SIZE);
     return 0; 
-}
-
-// Handles cases where if the pipe is full, child process segmented row data gets processed in multiple read calls.
-void safe_read(int fd, void *buf, size_t count) {
-    size_t total_read = 0;
-    while (total_read < count) {
-        // If row data is segmented, loop until child with write access adds remaining row data into pipe.
-        ssize_t n = read(fd, (char*)buf + total_read, count - total_read);
-        if (n <= 0) break; 
-        total_read += n;
-    }
 }
 
 // Instead of using macros, I made it easier to just include arguments into the pipe program.
 void arg_check(int argc, char *argv[]) {
-     if (argc <= 3) {
-        printf("Usage: %s [MATRIX_SIZE] [WORKERS] [USE_TRANSPOSE]\n", argv[0]);
-        printf("---------------------------------------------------------------------------------\n");
-        printf("MATRIX_SIZE - Set a matrix size                                     (2 - 10000)\n");
-        printf("WORKERS - Set # of child processes. 0 for # of logical processors.  (0 - INT_MAX)\n");
-        printf("USE_TRANSPOSE - Transposes posterior matrix to avoid cache misses.  (true or false)\n");
+     if (argc <= 1) {
+        printf("Usage: %s [MATRIX_SIZE]\n", argv[0]);
+        printf("------------------------------------------------\n");
+        printf("MATRIX_SIZE - Set a matrix size      (2 - 10000)\n");
         exit(1); 
     }
 
     if (atoi(argv[1]) < 2 || atoi(argv[1]) > 10000) {
         printf("Incompatible MATRIX_SIZE! (2 - 10000)\n");
         exit(1); 
-    }
-
-    if (atoi(argv[2]) < 0) {
-        printf("Incompatible WORKERS! (0 - INT_MAX)\n");
-        exit(1); 
-    }
-
-    if (strcmp(argv[3], "true") != 0 && strcmp(argv[3], "false") != 0) {
-        printf("Incompatible USE_TRANSPOSE! (true or false)\n");
-        exit(1);
     }
 }
